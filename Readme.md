2072da766741d267134e65b42ba4d4f4b31df7de
ISRA ILYAS <israqureshi138@gmail.com>
2024-11-08 22:03:36 +0500

    added project

---
+++ Readme.md
@@ -0,0 +1,20 @@
+# COmmands Used
+
+- npm init -y
+- tsc --init
+- npm i inquirer
+- npm i @types/inquirer -D
+
+### In tsconfig
+- target:2022
+- "module":"NodeNext"
+- "moduleResolution": "NodeNext"
+
+### In Package.json
+- "type":"module"
+
+## Inquirer
+
+In TypeScript (and JavaScript), inquirer is a popular library used to create interactive command-line interfaces. It provides a way to prompt users for input in the terminal, making it helpful for building CLIs (Command Line Interfaces) and gathering user inputs dynamically.
+
+By using inquirer, you can ask users questions (like multiple choice, input, confirm, etc.), and the library will handle displaying prompts and collecting responses.
---
+++ index.ts
@@ -0,0 +1,47 @@
+import inquirer from "inquirer";
+
+const answers:{
+    numberOne:number,
+    numberTwo:number,
+    operator:string
+} = await inquirer.prompt([
+    {
+    type:"number",
+    name:"numberOne",
+    message:"Kindly enter your first number"
+    },
+    {
+    type:"number",
+    name:"numberTwo",
+    message:"Kindly enter your first number"
+    },
+    {
+    type:"list",// multiple choice
+    name:"operator",
+    choices:["*","+","-","/"],
+    message:"Select Operator"
+    }
+])
+
+// DeStructure 
+const {numberOne,numberTwo,operator} = answers;
+
+if(numberOne && numberTwo && operator){
+    let result:number = 0;
+    if(operator == "+"){
+        result = numberOne+numberTwo;
+    }
+    else if(operator == "*"){
+        result = numberOne*numberTwo;
+    }
+    else if(operator == "/"){
+        result = numberOne/numberTwo;
+    }
+    else if(operator == "-"){
+        result = numberOne-numberTwo;
+    }
+    console.log("Your result is: ",result);
+}
+else{
+    console.log("Kindly enter valid input.")
+}
